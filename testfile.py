73e984039855fef9efe2be55de4a4c4e464f06da
Tandon Jenkins <tjdjj12@gmail.com>
2024-02-02 18:26:54 +0000

    test change

--- testfile.py
+++ testfile.py
@@ -1,1 +1,14 @@
-print("Hi new branch")
+phonebook = {  
+    "John" : 938477566,
+    "Jack" : 938377264,
+    "Jill" : 947662781
+}  
+# your code goes here
+phonebook[3] = "Jake"
+
+# testing code
+if "Jake" in phonebook:  
+    print("Jake is listed in the phonebook.")
+    
+if "Jill" not in phonebook:      
+    print("Jill is not listed in the phonebook at all.")  
